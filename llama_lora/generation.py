1be5b2d33c8be85ce100a114cc199f10f008609f
2023131031-yonsei-github <2023131031@yonsei.ac.kr>
2024-10-28 16:54:19 +0900

    Update generation.py

--- llama_lora/generation.py
+++ llama_lora/generation.py
@@ -3,13 +3,46 @@ import os
 import sys
 import time
 from pathlib import Path
-from typing import List, Optional, Tuple, TypedDict
+from typing import List, Literal, Optional, Tuple, TypedDict
 
 import torch
 import torch.nn.functional as F
+from fairscale.nn.model_parallel.initialize import (
+    get_model_parallel_rank,
+    initialize_model_parallel,
+    model_parallel_is_initialized,
+)
 
 from memory_transformer.llama_lora.model import ModelArgs, Transformer
 from memory_transformer.llama_lora.tokenizer import ChatFormat, Dialog, Message, Tokenizer
+
+Role = Literal["system", "user", "assistant"]
+
+
+class Message(TypedDict):
+    role: Role
+    content: str
+
+
+class CompletionPrediction(TypedDict, total=False):
+    generation: str
+    tokens: List[str]  # not required
+    logprobs: List[float]  # not required
+
+
+class ChatPrediction(TypedDict, total=False):
+    generation: Message
+    tokens: List[str]  # not required
+    logprobs: List[float]  # not required
+
+
+Dialog = List[Message]
+
+B_INST, E_INST = "[INST]", "[/INST]"
+B_SYS, E_SYS = "<<SYS>>\n", "\n<</SYS>>\n\n"
+
+SPECIAL_TAGS = [B_INST, E_INST, "<<SYS>>", "<</SYS>>"]
+UNSAFE_ERROR = "Error: special tags are not allowed as part of the prompt."
 
 class Llama:
     @staticmethod
